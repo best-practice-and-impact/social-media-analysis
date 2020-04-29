cb10de81f978c5b636065b7728d3585588d6ceb7
Joshua Halls <joshuah40@googlemail.com>
2020-04-29 09:51:58 +0100

    pandas

--- exploratory.py
+++ exploratory.py
@@ -5,14 +5,22 @@ Reddit exploatory script
 """
 
 import praw
+import pandas as pd
 
 reddit = praw.Reddit(client_id='RNGhJE66F0dfcg', 
                      client_secret='reMSAKkNv5daoHSRG3Cy15BhVw8', 
-                     user_agent='cdc')
+                     user_agent='cdc',)
 
 
-no_subreddit = reddit.subreddit('rstats')
-hot = no_subreddit.hot(limit=1000)
+subreddit2 = reddit.subreddit('rstats')
+r_subreddit = subreddit2.new(limit=1000)
+    
+reddit_data = []
 
-for submission in hot:
-    print(submission.title, submission.permalink)
+for i in r_subreddit:
+    reddit_data.append(
+        [i.title]
+        )
+
+df = pd.DataFrame(reddit_data)
+
